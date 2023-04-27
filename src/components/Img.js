c337d7799fa744ed7b058e06719bc851b4aff270
Eva Li <evali3101@gmail.com>
2023-04-27 12:33:04 -0400

    edited img page

--- src/components/Img.js
+++ src/components/Img.js
@@ -3,14 +3,81 @@ import axios from "axios";
 import { useState } from "react";
 
 const Img = () => {
-    const [response, setResponse] = useState(null);
-    const [desc, setDesc] = useState("");
+  const [response, setResponse] = useState(null);
+  const [desc, setDesc] = useState("");
 
-    return (
-        <div>
-            <h1>Images</h1>
-        </div>
-    )
-}
+  const fetchImg = async () => {
+    try {
+      const res = await axios.get(
+        "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI",
+        {
+          headers: {
+            "X-RapidAPI-Key":
+              "4c2350f8fdmsh67c94cfd9be6a99p12e932jsn8bf982d7a5dd",
+            "X-RapidAPI-Host":
+              "contextualwebsearch-websearch-v1.p.rapidapi.com",
+          },
+          params: {
+            q: desc,
+            pageNumber: "1",
+            pageSize: "10",
+            autoCorrect: "true",
+          },
+        }
+      );
+      setResponse(res.data.value);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const search = async (evt) => {
+    try {
+      if (evt.key === "Enter") {
+        evt.preventDefault();
+        fetchImg();
+        console.log("results --->", response);
+        setDesc("");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Images</h1>
+      <div>
+        <input
+          value={desc}
+          onChange={(event) => setDesc(event.target.value)}
+          onKeyPress={search}
+          type="text"
+          required
+        />
+      </div>
+      <section>
+        {response != null ? (
+          <div>
+            {response.map((element) => {
+              return (
+                <section key={element.url}>
+                  {/* <a href={element.url}>{element.title}</a> */}
+                  <a href={element.url}>
+                    <img alt="" src={element.url} />
+                  </a>
+                  {/* <span>{element.url}</span> */}
+                  {/* <img alt="" src={element.url} /> */}
+                </section>
+              );
+            })}
+          </div>
+        ) : (
+          "No information found... :("
+        )}
+      </section>
+    </div>
+  );
+};
 
 export default Img;
